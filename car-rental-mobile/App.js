d5e4cc31f69a1d2437004a90ba71bdd1548b7bd4
valo0n <valonnura888@outlook.com>
2026-06-07 07:35:57 +0200

    Mobile: StripeProvider dhe konfigurimi i app.json

--- car-rental-mobile/App.js
+++ car-rental-mobile/App.js
@@ -1,6 +1,17 @@
-import React from 'react';
-import AppNavigator from './src/navigation/AppNavigator';
+import React from "react";
+import { StripeProvider } from "@stripe/stripe-react-native";
+import AppNavigator from "./src/navigation/AppNavigator";
+
+// Vendos publishable key-in test (pk_test_...) ketu ose te .env si
+// EXPO_PUBLIC_STRIPE_PUBLISHABLE_KEY
+const STRIPE_PUBLISHABLE_KEY =
+  process.env.EXPO_PUBLIC_STRIPE_PUBLISHABLE_KEY ||
+  "pk_test_51TdrE03pznhVTsfxaVxjGpCovilAiESMikrYJK6mk7DejdVDE4tTJM7uDzHG9VaC85VeIDOCADzWRuBPX9VFfeyu008t12soCX";
 
 export default function App() {
-  return <AppNavigator />;
+  return (
+    <StripeProvider publishableKey={STRIPE_PUBLISHABLE_KEY}>
+      <AppNavigator />
+    </StripeProvider>
+  );
 }
